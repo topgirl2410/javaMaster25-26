c723cef7d4cb00f770b07d34928941d8e68c6be0
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 16:27:25 +0100

    nombre por consola

--- javaMaster/src/javaMaster/Nombre.java
+++ javaMaster/src/javaMaster/Nombre.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Nombre {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Dime como te llamas:");
