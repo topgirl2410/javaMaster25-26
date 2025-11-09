58615a63560358b7f45c5edf72923e14fae63dbb
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 16:39:27 +0100

    Acabando practicas del nombre por consola

--- javaMaster/src/javaMaster/Nombre.java
+++ javaMaster/src/javaMaster/Nombre.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class Nombre {
 
 	public static void main(String[] args) {
-	
+
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Dime como te llamas:");
 
 		String name = sc.nextLine();
-
 		System.out.println("¡Hola, " + name + "!");
+
+		System.out.print("En que ano estamos:");
+		int ano = sc.nextInt();
+		System.out.println("Estamos en el ano: " + ano);
+
+		sc.nextLine();
+
+		System.out.print("Que programacion te gusta mas: ");
+		String java = sc.nextLine();
+		System.out.println(java);
+
+		System.out.println("¡Hola " + name + " en este ano " + ano + " maseras una gran programadora en " + java);
 	}
 
 }
