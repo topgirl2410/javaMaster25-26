9239024c52c7bfeb491d8680f6b3d966ddf9b21a
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 16:25:13 +0100

    pedir tu nombre por consola

---
+++ javaMaster/src/javaMaster/Nombre.java
@@ -0,0 +1,18 @@
+package javaMaster;
+
+import java.util.Scanner;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Dime como te llamas:");
+
+		String name = sc.nextLine();
+
+		System.out.println("¡Hola, " + name + "!");
+	}
+
+}
