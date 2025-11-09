0f9ec204348acac4e9739bd5a54dd719b65baa15
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 19:31:07 +0100

    Caja Registradora

---
+++ javaMaster/src/buclesFor/CajaRegistradora.java
@@ -0,0 +1,42 @@
+package buclesFor;
+
+import java.util.Scanner;
+
+public class CajaRegistradora {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double total = 0;
+		double cantidad = -1; // Valor inicial distinto al 0 para entrar al bucle
+
+		System.out.println("=== Simulacíon de Caja Registradora ===");
+
+		// Usamos un for sin limite definido en la condición
+		for (; cantidad != 0;) {
+			System.out.println("Introduce el precio del producto (0 para finalizar)");
+			cantidad = sc.nextDouble();
+
+			if (cantidad != 0) {
+				total += cantidad;
+				System.out.println("Subtotal actual: $" + total);
+			}
+		}
+		System.out.println("-----------------------------------");
+		System.out.println("Total de la compra: $" + total);
+
+		if (total > 0) {
+			System.out.println("¿Con cuanto dinero paga el cliente?: ");
+			double pago = sc.nextDouble();
+
+			if (pago >= total) {
+				double cambio = pago - total;
+				System.out.println("Cambio a devolver: $" + cambio);
+			} else {
+				System.out.println("El pago no es suficiente. Faltan $" + (total - pago));
+			}
+		}
+
+		System.out.println("Gracias por su compra. ¡Vuelva pronto!");
+		sc.close();
+	}
+}
