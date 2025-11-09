05c4f7a38474d9bba23978c1cc02a5b7877c0a56
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 16:19:07 +0100

    hola mundo desde java

--- javaMaster/src/javaMaster/Hola.java
+++ javaMaster/src/javaMaster/Hola.java
@@ -4,7 +4,8 @@ public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String hola = "Hola mundo desde java";
+		System.out.println(hola);
 	}
 
 }
