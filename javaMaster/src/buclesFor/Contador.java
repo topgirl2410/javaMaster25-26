02f25bc70981c6724d6e5b2f4b9fcb0212311ec7
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-09 19:13:18 +0100

    COntador en java del 1 al 10

---
+++ javaMaster/src/buclesFor/Contador.java
@@ -0,0 +1,14 @@
+package buclesFor;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		// Bucle for que cuenta del 1 al 10
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Contador: " + i);
+		}
+		System.out.println("¡Fin de conteo!");
+	}
+
+}
