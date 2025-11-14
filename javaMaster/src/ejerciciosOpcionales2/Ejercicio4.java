57822b895352c2a7ccc86971a345d12d93c04545
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-14 19:29:39 +0100

    estado

---
+++ javaMaster/src/ejerciciosOpcionales2/Ejercicio4.java
@@ -0,0 +1,25 @@
+package ejerciciosOpcionales2;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char estado = 's';
+
+		switch (estado) {
+		case 's':
+			System.out.println("Soltero");
+			break;
+		case 'd':
+			System.out.println("Divorciado");
+			break;
+		case 'v':
+			System.out.println("Viudo");
+			break;
+		case 'c':
+			System.out.println("Casado");
+			break;
+		}
+	}
+
+}
