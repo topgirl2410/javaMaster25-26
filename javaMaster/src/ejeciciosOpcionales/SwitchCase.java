31c821c62e835e60c5994144cae5ea7eb5575c86
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-14 18:49:30 +0100

    dias de la semana

---
+++ javaMaster/src/ejeciciosOpcionales/SwitchCase.java
@@ -0,0 +1,36 @@
+package ejeciciosOpcionales;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dia = 5;
+		switch (dia) {
+		case 1:
+			System.out.println("Es lunes");
+			break;
+		case 2:
+			System.out.println("Es martes");
+			break;
+		case 3:
+			System.out.println("Es miercoles");
+			break;
+		case 4:
+			System.out.println("Es jueves");
+			break;
+		case 5:
+			System.out.println("Es viernes");
+			break;
+		case 6:
+			System.out.println("Es sabado");
+			break;
+		case 7:
+			System.out.println("Es domingo");
+			break;
+		default:
+			System.out.println("Ese dia no existe");
+		}
+
+	}
+
+}
