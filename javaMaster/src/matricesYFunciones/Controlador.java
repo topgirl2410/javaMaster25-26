8174b5217900cd98641053afd2cc4b89a0d06c4d
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-18 19:57:07 +0100

    barcos hundidos

--- javaMaster/src/matricesYFunciones/Controlador.java
+++ javaMaster/src/matricesYFunciones/Controlador.java
@@ -1,12 +1,22 @@
 package matricesYFunciones;
 
+import java.util.Scanner;
+
 public class Controlador {
 
 	public static void main(String[] args) {
-		int[][] tablero = { { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-				{ 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
-				{ 0, 0, -1, 0, 0, 0, 0, 0, 0, 0 }, { 1, 1, 0, 0, 0, 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-				{ 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+		int[][] tablero = {
+				{ 0, 0, 0, 0, 1, 1, 1, 1, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 9, 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, -1, 0, 0, 0, 0, 0, 0, 0 },
+				{ 1, 1, 0, 0, 0, 0, 0, 0, 1, 1 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				};
 		for (int i = 0; i < tablero.length; i++) {
 
 			for (int j = 0; j < tablero[i].length; j++) {
@@ -21,6 +31,29 @@ public class Controlador {
 				}
 			}
 			System.out.println("");
+		}
+		Scanner sc = new Scanner(System.in);
+		int aciertos = 0;
+		System.out.print("Introduce fila (0 - 9): ");
+		int fila = sc.nextInt();
+
+		System.out.print("Introduce columna (0 - 9): ");
+		int columna = sc.nextInt();
+
+		if (tablero[fila][columna] == 0) {
+			System.out.println("Agua");
+			tablero[fila][columna] = -1;
+			
+		}else if(tablero[fila][columna] == 1) {
+			System.out.println("Tocado");
+			tablero[fila][columna] = 9;
+			aciertos++;
+			
+		} else if(tablero[fila][columna] == 9){
+			System.out.println("Deja de gastar municion que ya le has dado");
+			
+		} else {
+			System.out.println("Ese tiro ya lo hiciste");
 		}
 	}
 
