a1fb43453ef4eca47ac355a0a54ce31c7c588380
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-18 19:35:12 +0100

    juego del barco

--- javaMaster/src/matricesYFunciones/Controlador.java
+++ javaMaster/src/matricesYFunciones/Controlador.java
@@ -5,17 +5,19 @@ public class Controlador {
 	public static void main(String[] args) {
 		int[][] tablero = { { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 				{ 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 1, 1, 0, 0, 0, 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, -1, 0, 0, 0, 0, 0, 0, 0 }, { 1, 1, 0, 0, 0, 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 				{ 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 		for (int i = 0; i < tablero.length; i++) {
 
 			for (int j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] == 0) {
+				if (tablero[i][j] == 0) {
 					System.out.print(" ~ ");
 				}
-				
-				if(tablero[i][j] == 1) {
+
+				else if (tablero[i][j] == 1) {
 					System.out.print(" B ");
+				} else {
+					System.out.print(" X");
 				}
 			}
 			System.out.println("");
