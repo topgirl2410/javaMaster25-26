1cc62250cac967030264429b16f5d86b343bcd80
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-18 20:36:13 +0100

    juego hundir la flota mejorado

--- javaMaster/src/matricesYFunciones/HundirLaFlota.java
+++ javaMaster/src/matricesYFunciones/HundirLaFlota.java
@@ -5,165 +5,154 @@ import java.util.Scanner;
 
 public class HundirLaFlota {
 
-	// Constantes del tablero
 	static final int AGUA = 0;
 	static final int BARCO = 1;
-	static final int TOCADO = 2;
-	static final int HUNDIDO = 3;
-	static final int FALLO = 4;
+	static final int FALLO = 2;
+	static final int TOCADO = 3;
+	static final int HUNDIDO = 4;
 
 	static Scanner sc = new Scanner(System.in);
 	static Random rnd = new Random();
 
 	public static void main(String[] args) {
 
-		System.out.println("====== HUNDIR LA FLOTA ======");
+		System.out.println("====== HUNDIR LA FLOTA ======\n");
 
 		int tamaño = elegirNivel();
 		int[][] tablero = new int[tamaño][tamaño];
 
-		// Barcos según nivel
 		int[] barcos = tamaño == 8 ? new int[] { 3, 2, 2, 1, 1 }
-				: tamaño == 10 ? new int[] { 4, 3, 3, 2, 2, 1, 1 } : new int[] { 5, 4, 3, 3, 2, 2, 1, 1 };
+				: tamaño == 10 ? new int[] { 4, 3, 3, 2, 2, 1, 1 } : new int[] { 5, 4, 4, 3, 3, 2, 2, 1, 1 };
 
-		colocarBarcosAleatoriamente(tablero, barcos);
+		colocarBarcos(tablero, barcos);
 
 		int disparos = 0;
-		int barcosHundidos = 0;
-		int totalBarcos = barcos.length;
 
-		boolean terminado = false;
-
-		while (!terminado) {
+		while (true) {
 
 			imprimirTablero(tablero, false);
 
-			System.out.println("\nIntroduce coordenadas para disparar: ");
+			System.out.println("\nIntroduce coordenadas para disparar:");
 			int fila = pedirNumero("Fila (0-" + (tamaño - 1) + "): ", 0, tamaño - 1);
-			int columna = pedirNumero("Columna (0-" + (tamaño - 1) + "): ", 0, tamaño - 1);
+			int col = pedirNumero("Columna (0-" + (tamaño - 1) + "): ", 0, tamaño - 1);
 
 			disparos++;
 
-			int resultado = procesarDisparo(tablero, fila, columna);
+			int res = disparar(tablero, fila, col);
 
-			if (resultado == FALLO)
-				System.out.println("Agua");
-			else if (resultado == TOCADO)
-				System.out.println("¡Tocado!");
-			else if (resultado == HUNDIDO) {
-				barcosHundidos++;
-				System.out.println("🔥 ¡BARCO HUNDIDO! 🔥");
-			} else {
-				System.out.println("Coordenada ya disparada.");
+			switch (res) {
+			case FALLO -> System.out.println("Agua.");
+			case TOCADO -> System.out.println("¡Tocado!");
+			case HUNDIDO -> System.out.println("🔥 ¡BARCO HUNDIDO! 🔥");
+			case -1 -> System.out.println("Ya habías disparado aquí.");
 			}
 
-			// Comprobar fin del juego
-			if (barcosHundidos == totalBarcos)
-				terminado = true;
+			if (!quedanBarcos(tablero))
+				break;
 		}
 
-		// FIN DEL JUEGO
+		// JUEGO TERMINADO
 		System.out.println("\n====== ¡HAS GANADO! ======");
 		imprimirTablero(tablero, true);
 
 		System.out.println("\nEstadísticas:");
-		System.out.println("Disparos realizados: " + disparos);
-		System.out.println("Barcos hundidos: " + barcosHundidos + " / " + totalBarcos);
-		System.out.println("Precisión: " + (100 * barcosHundidos / disparos) + "%");
+		System.out.println("Total disparos: " + disparos);
 
-		System.out.println("\nGracias por jugar :)");
+		sc.close();
 	}
 
 	// ------------------------------------------------------------------
-	// -------------------------- MENÚ Y NIVELES -------------------------
+	// MENÚ
 	// ------------------------------------------------------------------
 
 	public static int elegirNivel() {
-		System.out.println("\nElige nivel:");
+		System.out.println("Elige nivel:");
 		System.out.println("1 - Fácil (8x8)");
 		System.out.println("2 - Medio (10x10)");
 		System.out.println("3 - Difícil (12x12)");
 
-		int opcion = pedirNumero("Opción: ", 1, 3);
+		int opt = pedirNumero("Opción: ", 1, 3);
 
-		return opcion == 1 ? 8 : opcion == 2 ? 10 : 12;
+		return opt == 1 ? 8 : opt == 2 ? 10 : 12;
 	}
 
 	// ------------------------------------------------------------------
-	// ----------------------- COLOCAR BARCOS ----------------------------
+	// COLOCAR BARCOS
 	// ------------------------------------------------------------------
 
-	public static void colocarBarcosAleatoriamente(int[][] tablero, int[] barcos) {
-		for (int tamañoBarco : barcos) {
+	public static void colocarBarcos(int[][] t, int[] barcos) {
+		for (int tamaño : barcos) {
 
 			boolean colocado = false;
 
 			while (!colocado) {
 
-				int fila = rnd.nextInt(tablero.length);
-				int col = rnd.nextInt(tablero.length);
+				int fila = rnd.nextInt(t.length);
+				int col = rnd.nextInt(t.length);
 				boolean horizontal = rnd.nextBoolean();
 
-				if (puedeColocar(tablero, fila, col, tamañoBarco, horizontal)) {
-					colocar(tablero, fila, col, tamañoBarco, horizontal);
+				if (puedeColocar(t, fila, col, tamaño, horizontal)) {
+					colocar(t, fila, col, tamaño, horizontal);
 					colocado = true;
 				}
 			}
 		}
 	}
 
-	public static boolean puedeColocar(int[][] tab, int fila, int col, int tamaño, boolean horizontal) {
+	public static boolean puedeColocar(int[][] t, int fila, int col, int size, boolean hor) {
 
-		if (horizontal) {
-			if (col + tamaño > tab.length)
+		if (hor) {
+			if (col + size > t.length)
 				return false;
-
-			for (int c = col; c < col + tamaño; c++)
-				if (tab[fila][c] != AGUA)
+			for (int c = col; c < col + size; c++)
+				if (t[fila][c] != AGUA)
 					return false;
 
 		} else {
-			if (fila + tamaño > tab.length)
+			if (fila + size > t.length)
 				return false;
-
-			for (int f = fila; f < fila + tamaño; f++)
-				if (tab[f][col] != AGUA)
+			for (int f = fila; f < fila + size; f++)
+				if (t[f][col] != AGUA)
 					return false;
 		}
 
 		return true;
 	}
 
-	public static void colocar(int[][] tab, int fila, int col, int tamaño, boolean horizontal) {
-		if (horizontal) {
-			for (int c = col; c < col + tamaño; c++)
-				tab[fila][c] = BARCO;
+	public static void colocar(int[][] t, int fila, int col, int size, boolean hor) {
+
+		if (hor) {
+			for (int c = col; c < col + size; c++)
+				t[fila][c] = BARCO;
+
 		} else {
-			for (int f = fila; f < fila + tamaño; f++)
-				tab[f][col] = BARCO;
+			for (int f = fila; f < fila + size; f++)
+				t[f][col] = BARCO;
 		}
 	}
 
 	// ------------------------------------------------------------------
-	// -------------------------- DISPAROS -------------------------------
+	// DISPAROS
 	// ------------------------------------------------------------------
 
-	public static int procesarDisparo(int[][] tab, int fila, int col) {
+	public static int disparar(int[][] t, int fila, int col) {
 
-		if (tab[fila][col] == AGUA) {
-			tab[fila][col] = FALLO;
+		if (t[fila][col] == AGUA) {
+			t[fila][col] = FALLO;
 			return FALLO;
 		}
-		if (tab[fila][col] == FALLO || tab[fila][col] == HUNDIDO || tab[fila][col] == TOCADO)
-			return -1;
 
-		if (tab[fila][col] == BARCO) {
-			tab[fila][col] = TOCADO;
+		if (t[fila][col] == FALLO || t[fila][col] == TOCADO || t[fila][col] == HUNDIDO)
+			return -1; // repetido
 
-			if (barcoHundido(tab, fila, col)) {
-				marcarHundido(tab, fila, col);
+		if (t[fila][col] == BARCO) {
+			t[fila][col] = TOCADO;
+
+			if (barcoEnteroHundido(t, fila, col)) {
+				hundirBarco(t, fila, col);
 				return HUNDIDO;
 			}
+
 			return TOCADO;
 		}
 
@@ -171,60 +160,102 @@ public class HundirLaFlota {
 	}
 
 	// ------------------------------------------------------------------
-	// ------------ COMPROBAR SI UN BARCO ENTERO ESTÁ HUNDIDO ----------
+	// DETECTAR SI EL BARCO ESTÁ COMPLETO
 	// ------------------------------------------------------------------
 
-	public static boolean barcoHundido(int[][] tab, int fila, int col) {
-		return comprobarHorizontal(tab, fila, col) && comprobarVertical(tab, fila, col);
-	}
+	public static boolean barcoEnteroHundido(int[][] t, int fila, int col) {
 
-	public static boolean comprobarHorizontal(int[][] tab, int fila, int col) {
-		// Izquierda
-		for (int c = col; c >= 0; c--)
-			if (tab[fila][c] == BARCO)
-				return false;
+		// Extender horizontalmente
+		int c = col;
+		while (c >= 0 && esParteBarco(t[fila][c]))
+			c--;
+		int izquierda = c + 1;
 
-		// Derecha
-		for (int c = col; c < tab.length; c++)
-			if (tab[fila][c] == BARCO)
-				return false;
+		c = col;
+		while (c < t.length && esParteBarco(t[fila][c]))
+			c++;
+		int derecha = c - 1;
 
+		// Extender verticalmente
+		int f = fila;
+		while (f >= 0 && esParteBarco(t[f][col]))
+			f--;
+		int arriba = f + 1;
+
+		f = fila;
+		while (f < t.length && esParteBarco(t[f][col]))
+			f++;
+		int abajo = f - 1;
+
+		// Buscar horizontal
+		if (izquierda != derecha) {
+			for (int x = izquierda; x <= derecha; x++)
+				if (t[fila][x] == BARCO)
+					return false;
+			return true;
+		}
+
+		// Buscar vertical
+		if (arriba != abajo) {
+			for (int x = arriba; x <= abajo; x++)
+				if (t[x][col] == BARCO)
+					return false;
+			return true;
+		}
+
+		// Barco de 1 celda
 		return true;
 	}
 
-	public static boolean comprobarVertical(int[][] tab, int fila, int col) {
-		// Arriba
-		for (int f = fila; f >= 0; f--)
-			if (tab[f][col] == BARCO)
-				return false;
-
-		// Abajo
-		for (int f = fila; f < tab.length; f++)
-			if (tab[f][col] == BARCO)
-				return false;
-
-		return true;
-	}
-
-	public static void marcarHundido(int[][] tab, int fila, int col) {
-
-		// Horizontal
-		for (int c = col; c >= 0 && tab[fila][c] != AGUA && tab[fila][c] != FALLO; c--)
-			tab[fila][c] = HUNDIDO;
-
-		for (int c = col; c < tab.length && tab[fila][c] != AGUA && tab[fila][c] != FALLO; c++)
-			tab[fila][c] = HUNDIDO;
-
-		// Vertical
-		for (int f = fila; f >= 0 && tab[f][col] != AGUA && tab[f][col] != FALLO; f--)
-			tab[f][col] = HUNDIDO;
-
-		for (int f = fila; f < tab.length && tab[f][col] != AGUA && tab[f][col] != FALLO; f++)
-			tab[f][col] = HUNDIDO;
+	public static boolean esParteBarco(int valor) {
+		return valor == BARCO || valor == TOCADO || valor == HUNDIDO;
 	}
 
 	// ------------------------------------------------------------------
-	// ----------------------------- UTILS -------------------------------
+	// MARCAR COMO HUNDIDO TODAS LAS CELDAS DEL BARCO
+	// ------------------------------------------------------------------
+
+	public static void hundirBarco(int[][] t, int fila, int col) {
+
+		// horizontal
+		int c = col;
+		while (c >= 0 && esParteBarco(t[fila][c])) {
+			t[fila][c] = HUNDIDO;
+			c--;
+		}
+		c = col;
+		while (c < t.length && esParteBarco(t[fila][c])) {
+			t[fila][c] = HUNDIDO;
+			c++;
+		}
+
+		// vertical
+		int f = fila;
+		while (f >= 0 && esParteBarco(t[f][col])) {
+			t[f][col] = HUNDIDO;
+			f--;
+		}
+		f = fila;
+		while (f < t.length && esParteBarco(t[f][col])) {
+			t[f][col] = HUNDIDO;
+			f++;
+		}
+	}
+
+	// ------------------------------------------------------------------
+	// COMPROBAR SI QUEDAN BARCOS
+	// ------------------------------------------------------------------
+
+	public static boolean quedanBarcos(int[][] t) {
+		for (int[] fila : t)
+			for (int celda : fila)
+				if (celda == BARCO || celda == TOCADO)
+					return true;
+		return false;
+	}
+
+	// ------------------------------------------------------------------
+	// UTILS
 	// ------------------------------------------------------------------
 
 	public static int pedirNumero(String msg, int min, int max) {
@@ -240,26 +271,28 @@ public class HundirLaFlota {
 		return num;
 	}
 
-	public static void imprimirTablero(int[][] tab, boolean mostrarBarcos) {
-		System.out.println("\n    0 1 2 3 4 5 6 7 8 9");
-		System.out.println("   ---------------------");
+	public static void imprimirTablero(int[][] t, boolean mostrarBarcos) {
 
-		for (int i = 0; i < tab.length; i++) {
+		System.out.print("\n   ");
+		for (int i = 0; i < t.length; i++)
+			System.out.print(i + " ");
+		System.out.println("\n   " + "- ".repeat(t.length));
+
+		for (int i = 0; i < t.length; i++) {
 			System.out.print(i + " | ");
-			for (int j = 0; j < tab[i].length; j++) {
+			for (int j = 0; j < t[i].length; j++) {
 
-				int celda = tab[i][j];
+				int c = t[i][j];
 
-				if (!mostrarBarcos && celda == BARCO) {
+				if (!mostrarBarcos && c == BARCO) {
 					System.out.print("~ ");
-				} else if (celda == AGUA || celda == BARCO) {
-					System.out.print("~ ");
-				} else if (celda == FALLO) {
-					System.out.print("x ");
-				} else if (celda == TOCADO) {
-					System.out.print("* ");
-				} else if (celda == HUNDIDO) {
-					System.out.print("# ");
+				} else {
+					switch (c) {
+					case AGUA, BARCO -> System.out.print("~ ");
+					case FALLO -> System.out.print("x ");
+					case TOCADO -> System.out.print("* ");
+					case HUNDIDO -> System.out.print("# ");
+					}
 				}
 			}
 			System.out.println();
